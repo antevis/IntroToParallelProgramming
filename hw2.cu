d7cb6a835e15b23c6fe0342577753ee54fd3e4b0
antevis <polyconnect@me.com>
2017-01-02 17:47:46 +0200

    bug fix

--- hw2.cu
+++ hw2.cu
@@ -241,10 +241,10 @@ void your_gaussian_blur(const uchar4 * const h_inputImageRGBA, uchar4 * const d_
   //TODO:
   //Compute correct grid size (i.e., number of blocks per kernel launch)
   //from the image size and and block size.
-  const dim3 gridSize(numCols/blockDimX + 1,numRows/BlockSizeY + 1, 1);
+  const dim3 gridSize(numCols/blockDimX + 1,numRows/blockDimY + 1, 1);
 
   //TODO: Launch a kernel for separating the RGBA image into different color channels
-  separateChannels<<gridSize, blockSize>>(d_inputImageRGBA,
+  separateChannels<<<gridSize, blockSize>>>(d_inputImageRGBA,
                        numRows,
                        numCols,
                        d_red,
@@ -256,9 +256,9 @@ void your_gaussian_blur(const uchar4 * const h_inputImageRGBA, uchar4 * const d_
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 
   //TODO: Call your convolution kernel here 3 times, once for each color channel.
-  gaussian_blur<<gridSize, blockSize>>(d_red, d_redBlurred, numRows, numCols, d_filter, filterWidth);
-  gaussian_blur<<gridSize, blockSize>>(d_green, d_greenBlurred, numRows, numCols, d_filter, filterWidth);
-  gaussian_blur<<gridSize, blockSize>>(d_blue, d_blueBlurred, numRows, numCols, d_filter, filterWidth);
+  gaussian_blur<<<gridSize, blockSize>>>(d_red, d_redBlurred, numRows, numCols, d_filter, filterWidth);
+  gaussian_blur<<<gridSize, blockSize>>>(d_green, d_greenBlurred, numRows, numCols, d_filter, filterWidth);
+  gaussian_blur<<<gridSize, blockSize>>>(d_blue, d_blueBlurred, numRows, numCols, d_filter, filterWidth);
 
   // Again, call cudaDeviceSynchronize(), then call checkCudaErrors() immediately after
   // launching your kernel to make sure that you didn't make any mistakes.
@@ -285,5 +285,4 @@ void cleanup() {
   checkCudaErrors(cudaFree(d_green));
   checkCudaErrors(cudaFree(d_blue));
   checkCudaErrors(cudaFree(d_filter));
-  
 }
