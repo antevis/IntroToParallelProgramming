96cb8e583afcda799480cf2421ac116c04b529ef
antevis <polyconnect@me.com>
2017-01-12 17:44:14 +0300

    debugged

--- hw3.cu
+++ hw3.cu
@@ -1,12 +1,16 @@
 /* Udacity Homework 3
    HDR Tone-mapping
+
   Background HDR
   ==============
+
   A High Dynamic Range (HDR) image contains a wider variation of intensity
   and color than is allowed by the RGB format with 1 byte per channel that we
   have used in the previous assignment.  
+
   To store this extra information we use single precision floating point for
   each channel.  This allows for an extremely wide range of intensity values.
+
   In the image for this assignment, the inside of church with light coming in
   through stained glass windows, the raw input floating point values for the
   channels range from 0 to 275.  But the mean is .41 and 98% of the values are
@@ -15,48 +19,64 @@
   [0-255] range that we have been using then most values will be mapped to zero!
   The only thing we will be able to see are the very brightest areas - the
   windows - everything else will appear pitch black.
+
   The problem is that although we have cameras capable of recording the wide
   range of intensity that exists in the real world our monitors are not capable
   of displaying them.  Our eyes are also quite capable of observing a much wider
   range of intensities than our image formats / monitors are capable of
   displaying.
+
   Tone-mapping is a process that transforms the intensities in the image so that
   the brightest values aren't nearly so far away from the mean.  That way when
   we transform the values into [0-255] we can actually see the entire image.
   There are many ways to perform this process and it is as much an art as a
   science - there is no single "right" answer.  In this homework we will
   implement one possible technique.
+
   Background Chrominance-Luminance
   ================================
+
   The RGB space that we have been using to represent images can be thought of as
   one possible set of axes spanning a three dimensional space of color.  We
   sometimes choose other axes to represent this space because they make certain
   operations more convenient.
+
   Another possible way of representing a color image is to separate the color
   information (chromaticity) from the brightness information.  There are
   multiple different methods for doing this - a common one during the analog
   television days was known as Chrominance-Luminance or YUV.
+
   We choose to represent the image in this way so that we can remap only the
   intensity channel and then recombine the new intensity values with the color
   information to form the final image.
+
   Old TV signals used to be transmitted in this way so that black & white
   televisions could display the luminance channel while color televisions would
   display all three of the channels.
   
+
   Tone-mapping
   ============
+
   In this assignment we are going to transform the luminance channel (actually
   the log of the luminance, but this is unimportant for the parts of the
   algorithm that you will be implementing) by compressing its range to [0, 1].
   To do this we need the cumulative distribution of the luminance values.
+
   Example
   -------
+
   input : [2 4 3 3 1 7 4 5 7 0 9 4 3 2]
   min / max / range: 0 / 9 / 9
+
   histo with 3 bins: [4 7 3]
+
   cdf : [4 11 14]
+
+
   Your task is to calculate this cumulative distribution by following these
   steps.
+
 */
 
 #include <limits.h>
@@ -64,6 +84,8 @@
 #include <math.h>
 #include <stdio.h>
 
+
+#include "reference_calc.cpp"
 #include "utils.h"
 
 __global__
@@ -168,14 +190,14 @@ float reduceMinmax(const float* const d_in, const size_t numRows, const size_t n
     dim3 thread_dim(BLOCK_SIZE);
     const int shared_mem_size = sizeof(float)*BLOCK_SIZE;
     
-    while(True) {
-        checkCudaErrors(cudaMalloc(&d_curr_out, sizeof(float) * getMaxSize(curr_size, BLOCK_SIZE)));
+    while(true) {
+        checkCudaErrors(cudaMalloc(&d_curr_out, sizeof(float) * getMaxSize(currSize, BLOCK_SIZE)));
         
         dim3 block_dim(getMaxSize(size, BLOCK_SIZE));
         reduce_minmax_kernel<<<block_dim, thread_dim, shared_mem_size>>>(
             d_curr_in,
             d_curr_out,
-            curr_size,
+            currSize,
             minmax
         );
         cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
@@ -185,10 +207,10 @@ float reduceMinmax(const float* const d_in, const size_t numRows, const size_t n
         checkCudaErrors(cudaFree(d_curr_in));
         d_curr_in = d_curr_out;
         
-        if(curr_size <  BLOCK_SIZE) 
+        if(currSize <  BLOCK_SIZE) 
             break;
         
-        curr_size = getMaxSize(curr_size, BLOCK_SIZE);
+        currSize = getMaxSize(currSize, BLOCK_SIZE);
     }
     
     // theoretically we should be 
@@ -206,45 +228,6 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
                                   const size_t numCols,
                                   const size_t numBins)
 {
-    const size_t size = numRows*numCols;
-    min_logLum = reduceMinmax(d_logLuminance, numRows, numCols, 0);
-    max_logLum = reduceMinmax(d_logLuminance, numRows, numCols, 1);
-    
-    printf("got min of %f\n", min_logLum);
-    printf("got max of %f\n", max_logLum);
-    printf("numBins %d\n", numBins);
-    
-    unsigned int* d_bins;
-    size_t histo_size = sizeof(unsigned int)*numBins;
-
-    checkCudaErrors(cudaMalloc(&d_bins, histo_size));    
-    checkCudaErrors(cudaMemset(d_bins, 0, histo_size));  
-    dim3 thread_dim(1024);
-    dim3 hist_block_dim(get_max_size(size, thread_dim.x));
-    hist_kernel<<<hist_block_dim, thread_dim>>>(d_bins, d_logLuminance, numBins, min_logLum, max_logLum, numRows, numCols);
-    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
-
-    unsigned int h_out[100];
-    cudaMemcpy(&h_out, d_bins, sizeof(unsigned int)*100, cudaMemcpyDeviceToHost);
-    for(int i = 0; i < 100; i++)
-        printf("hist out %d\n", h_out[i]);
-    
-    dim3 scan_block_dim(get_max_size(numBins, thread_dim.x));
-
-    scan_kernel<<<scan_block_dim, thread_dim>>>(d_bins, numBins);
-    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
-    
-    cudaMemcpy(&h_out, d_bins, sizeof(unsigned int)*100, cudaMemcpyDeviceToHost);
-    for(int i = 0; i < 100; i++)
-        printf("cdf out %d\n", h_out[i]);
-    
-
-    cudaMemcpy(d_cdf, d_bins, histo_size, cudaMemcpyDeviceToDevice);
-
-    
-    checkCudaErrors(cudaFree(d_bins));
-     
-    
   //TODO
   /*Here are the steps you need to implement
     1) find the minimum and maximum value in the input logLuminance channel
@@ -255,4 +238,42 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
     4) Perform an exclusive scan (prefix sum) on the histogram to get
        the cumulative distribution of luminance values (this should go in the
        incoming d_cdf pointer which already has been allocated for you)       */
+       
+    const size_t size = numRows*numCols;
+    min_logLum = reduceMinmax(d_logLuminance, numRows, numCols, 0);
+    max_logLum = reduceMinmax(d_logLuminance, numRows, numCols, 1);
+    
+    //printf("got min of %f\n", min_logLum);
+    //printf("got max of %f\n", max_logLum);
+    //printf("numBins %d\n", numBins);
+    
+    unsigned int* d_bins;
+    size_t histo_size = sizeof(unsigned int)*numBins;
+
+    checkCudaErrors(cudaMalloc(&d_bins, histo_size));    
+    checkCudaErrors(cudaMemset(d_bins, 0, histo_size));  
+    dim3 thread_dim(1024);
+    dim3 hist_block_dim(getMaxSize(size, thread_dim.x));
+    hist_kernel<<<hist_block_dim, thread_dim>>>(d_bins, d_logLuminance, numBins, min_logLum, max_logLum, numRows, numCols);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+    unsigned int h_out[100];
+    cudaMemcpy(&h_out, d_bins, sizeof(unsigned int)*100, cudaMemcpyDeviceToHost);
+    //for(int i = 0; i < 100; i++)
+    //    printf("hist out %d\n", h_out[i]);
+    
+    dim3 scan_block_dim(getMaxSize(numBins, thread_dim.x));
+
+    scan_kernel<<<scan_block_dim, thread_dim>>>(d_bins, numBins);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    
+    cudaMemcpy(&h_out, d_bins, sizeof(unsigned int)*100, cudaMemcpyDeviceToHost);
+    //for(int i = 0; i < 100; i++)
+    //    printf("cdf out %d\n", h_out[i]);
+    
+
+    cudaMemcpy(d_cdf, d_bins, histo_size, cudaMemcpyDeviceToDevice);
+
+    
+    checkCudaErrors(cudaFree(d_bins));
 }
